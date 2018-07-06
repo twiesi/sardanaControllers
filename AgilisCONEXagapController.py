d1fbc3272e605947bb11f81356a48e0e50a35e63
Daniel Schick <schick.daniel@gmail.com>
2018-07-06 16:03:27 +0200

    add conexAGAP controller

---
+++ AgilisCONEXagapController.py
@@ -0,0 +1,89 @@
+##############################################################################
+##
+# This file is part of Sardana
+##
+# http://www.sardana-controls.org/
+##
+# Copyright 2011 CELLS / ALBA Synchrotron, Bellaterra, Spain
+##
+# Sardana is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+##
+# Sardana is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+##
+# You should have received a copy of the GNU Lesser General Public License
+# along with Sardana.  If not, see <http://www.gnu.org/licenses/>.
+##
+##############################################################################
+
+"""This file contains the code for an hypothetical Springfield motor controller
+used in documentation"""
+
+from pyagilis.controller import AGAP
+
+from sardana import State
+from sardana.pool.controller import MotorController
+from sardana.pool.controller import Type, Description, DefaultValue
+
+
+class AgilisCONEXagapController(MotorController):
+    ctrl_properties = {'port': {Type: str, Description: 'The port of the rs232 device', DefaultValue: '/dev/ttyAGILIS2'}}
+    
+    MaxDevice = 2
+    
+    def __init__(self, inst, props, *args, **kwargs):
+        super(AgilisCONEXagapController, self).__init__(
+            inst, props, *args, **kwargs)
+
+        # initialize hardware communication
+        self.agilis = AGAP(self.port)
+        if self.agilis.getStatus() == 0: # configuration mode
+            print('Controller is in configuration mode!')
+        # do some initialization
+        self._motors = {}
+
+    def AddDevice(self, axis):
+        self._motors[axis] = True
+
+    def DeleteDevice(self, axis):
+        del self._motors[axis]
+
+    StateMap = {
+        1: State.On,
+        2: State.Moving,
+        3: State.Fault,
+    }
+
+    def StateOne(self, axis):
+        limit_switches = MotorController.NoLimitSwitch     
+        state = self.agilis.getStatus()
+                
+        return self.StateMap[state], 'some text', limit_switches
+
+    def ReadOne(self, axis):
+        positions = self.agilis.getCurrentPosition()
+        return float(positions[axis])
+        
+    def PreStartAll(self):
+        # clear the local motion information dictionary
+        self._moveable_info = [0, 0]
+
+    def StartOne(self, axis, position):
+        # store information about this axis motion
+        self._moveable_info[axis] = position
+
+    def StartAll(self):
+        self.agilis.moveAbsolute(self._moveable_info[0], self._moveable_info[1])
+
+    def StopOne(self, axis):
+        self.agilis.stop()
+
+    def AbortOne(self, axis):
+        self.agilis.stop()
+
+    
